974f54118bc2880babe2f90d9e5f6c43b506a4ef
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:47:57 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "api_management_notification_recipient_users" {
+  description = "All api_management_notification_recipient_user resources"
+  value       = azurerm_api_management_notification_recipient_user.api_management_notification_recipient_users
+}
+
