1dc618c3604ce199b0ed87128f6c7859dff1f4f7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "api_management_notification_recipient_users" {
-  description = "All api_management_notification_recipient_user resources"
-  value       = azurerm_api_management_notification_recipient_user.api_management_notification_recipient_users
-}
 output "api_management_notification_recipient_users_api_management_id" {
-  description = "List of api_management_id values across all api_management_notification_recipient_users"
-  value       = [for k, v in azurerm_api_management_notification_recipient_user.api_management_notification_recipient_users : v.api_management_id]
+  description = "Map of api_management_id values across all api_management_notification_recipient_users, keyed the same as var.api_management_notification_recipient_users"
+  value       = { for k, v in azurerm_api_management_notification_recipient_user.api_management_notification_recipient_users : k => v.api_management_id }
 }
 output "api_management_notification_recipient_users_notification_type" {
-  description = "List of notification_type values across all api_management_notification_recipient_users"
-  value       = [for k, v in azurerm_api_management_notification_recipient_user.api_management_notification_recipient_users : v.notification_type]
+  description = "Map of notification_type values across all api_management_notification_recipient_users, keyed the same as var.api_management_notification_recipient_users"
+  value       = { for k, v in azurerm_api_management_notification_recipient_user.api_management_notification_recipient_users : k => v.notification_type }
 }
 output "api_management_notification_recipient_users_user_id" {
-  description = "List of user_id values across all api_management_notification_recipient_users"
-  value       = [for k, v in azurerm_api_management_notification_recipient_user.api_management_notification_recipient_users : v.user_id]
+  description = "Map of user_id values across all api_management_notification_recipient_users, keyed the same as var.api_management_notification_recipient_users"
+  value       = { for k, v in azurerm_api_management_notification_recipient_user.api_management_notification_recipient_users : k => v.user_id }
 }
 
